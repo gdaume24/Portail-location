0563fcc3213e698a693446101c78f9594c066484
Geoffroy <gdaumer@2cconseil.com>
2024-12-09 18:37:15 +0100

    Gestion des erreurs

--- Back/src/main/java/com/rentals/configs/SecurityConfiguration.java
+++ Back/src/main/java/com/rentals/configs/SecurityConfiguration.java
@@ -1,5 +1,7 @@
 package com.rentals.configs;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -12,7 +14,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.List;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Configuration
 @EnableWebSecurity
@@ -30,19 +32,22 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf()
-                .disable()
-                .authorizeHttpRequests()
-                .requestMatchers("/auth/**", "/rentals-photos/**", "/swagger-ui/**", "/swagger-ui.html", "v3/**")
-                .permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                    .requestMatchers("/auth/**", "/rentals-photos/**", "/swagger-ui/**", "/swagger-ui.html", "v3/**").permitAll()
+                    .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
                 .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling(exception -> exception
+                    .authenticationEntryPoint((request, response, authException) -> {
+                        // Gérer les clés périmées ou incorrectes
+                        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                    })
+                );
 
         return http.build();
     }
